760a7b81ba58d07a6f4ab2a8af1c286086926e0f
jlessoff <jlessoff@gmail.com>
2021-12-15 15:44:36 +0100

    Temporal Graph DF

--- Enron_Project_Data.py
+++ Enron_Project_Data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import csv
 events = pd.read_csv('Enron_events.txt', sep="	")
 positions=pd.read_csv('Enron_positions.txt', sep='	')
 positions.columns=['id','email','position']
@@ -7,18 +7,19 @@ from datetime import datetime
 pd.set_option("display.max_columns", None)
 # pd.set_option("display.max_rows", None)
 date=[]
+day_num=[]
 for i in events['time']:
     day=datetime.fromtimestamp(956834340 + i).strftime("%d-%m-%Y")
     day=datetime.strptime(day,"%d-%m-%Y")
     date.append(day)
-
+    print(day)
 events['date']=date
 
 ###merge data sets to give more information about senders
 new_df= pd.merge(
         events,
         positions,
-        how='right',
+        how='left',
         left_on='exped',
         right_on='id'
     )
@@ -30,7 +31,7 @@ new_df=new_df.drop('exped',axis=1)
 enron= pd.merge(
         new_df,
         positions,
-        how='right',
+        how='left',
         left_on='destin',
         right_on='id'
     )
@@ -67,54 +68,41 @@ profiles_agg=pd.merge(
 profiles_agg=profiles_agg.rename(columns={'sender_id':'id', 'sender_email':'email','sender_position':'position'})
 profiles_agg=profiles_agg[['id','email','position','count_sent','count_rec']]
 
+
 ###find average daily emails sent and recieved
 enron_daily_emails_sent = events.groupby([events['date'].dt.date,events['exped']])['destin'].count().reset_index().rename(columns={'destin':'daily_count_sent','exped' : 'employee_id'})
-
-
 enron_daily_emails_received = events.groupby([events['date'].dt.date,events['destin']])['exped'].count().reset_index().rename(columns={'destin':'employee_id','exped' : 'daily_count_received'})
-
 employee_email_stats_daily= pd.merge(
         enron_daily_emails_received,
         enron_daily_emails_sent,
         how='outer',
         on=['employee_id','date']
     )
-
 employee_email_stats_daily= pd.merge(
         employee_email_stats_daily,
         positions,
-        how='right',
+        how='left',
         left_on='employee_id',
         right_on='id'
     )
+
 #drop n/a from employee id
-employee_email_stats_daily = employee_email_stats_daily.dropna(subset=['employee_id'])
-employee_email_stats_average=employee_email_stats_daily.groupby(employee_email_stats_daily['employee_id'])['employee_id','daily_count_sent','daily_count_received'].mean()
-#how many employees are each interacting with?
-data = []
+employee_email_stats_daily = employee_email_stats_daily.fillna(0)
 
-sender_count=enron.groupby(['sender_id'])['destin_id'].nunique().reset_index(name="count_sent")
-receiver_count=enron.groupby(['destin_id'])['sender_id'].nunique().reset_index(name="count_sent")
-send_receive = pd.DataFrame(enron, columns=['sender_count', 'reciever_count'])
+employee_email_stats_daily = employee_email_stats_daily.drop('id',axis=1)
+employee_email_stats_daily = employee_email_stats_daily.drop('email',axis=1)
 
-print(employee_email_stats_average)
-sender= pd.merge(
-        receiver_count,
-        sender_count,
-        how='outer',
-        left_on='destin_id',
-        right_on = 'sender_id'
-)
-# summary_info= pd.merge(
-#         employee_email_stats_average,
-#         sender,
-#         how='left',
-#         left_on='employee_id',
-#         right_on = 'sender_id'
-# )
-# summary_info=employee_email_stats_average[['employee_id','daily_count_received','daily_count_sent','count_sent_x','count_sent_y']].fillna(0)
-print(employee_email_stats_average)
-#print(employee_email_stats_daily)
+employee_email_stats_average=employee_email_stats_daily.groupby(employee_email_stats_daily['employee_id'])['daily_count_sent','daily_count_received'].mean()
 #####
 graph_ds=enron.groupby(['sender_id','destin_id'])['time'].count().reset_index(name="count")
-#print(graph_ds)
+
+
+
+enron['date'] = pd.to_datetime(enron.date)
+basedate = pd.Timestamp('2000-04-27')
+# basedate = datetime.strptime(basedate, "%d-%m-%Y")
+enron['time_since'] = (enron['date']-basedate).dt.days
+graph_ds_2=enron.groupby(['time_since','sender_id','destin_id'])['time'].count().reset_index(name="count")
+time_graph_ds=new_df=graph_ds_2.rename(columns={'time_since':'time', 'sender_id':'source','destin_id':'target','count':'weight'})
+print(time_graph_ds)
+s=time_graph_ds.to_csv('time_graph.tedges')
